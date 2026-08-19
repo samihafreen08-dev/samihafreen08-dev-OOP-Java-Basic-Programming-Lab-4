2ce1cef470a82f02dce3634db6d69965e3d71992
samihafreen08-dev <samihafreen08@gmail.com>
2026-08-20 00:22:10 +0600

    Add files via upload

---
+++ while3.java
@@ -0,0 +1,11 @@
+public class while3 {
+    public static void main(String[] args) {
+
+        int i = 10;
+
+        while (i < 5) {
+            System.out.println("This will never be printed");
+            i++;
+        }
+    }
+}
